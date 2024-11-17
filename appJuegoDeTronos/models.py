888635f5fad5d4d1b2f7abe007da02741161e3eb
Diego Ramirez <diegoramirez@opendeusto.es>
2024-11-17 19:32:51 +0100

    todos los personajes ya metidos

--- appJuegoDeTronos/models.py
+++ appJuegoDeTronos/models.py
@@ -7,18 +7,18 @@ class House(models.Model):
     def __str__(self):
         return self.name
 
+class Character(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    house = models.ForeignKey('House', null=True, blank=True, on_delete=models.SET_NULL, related_name='characters')
+    seasons = models.ManyToManyField('Season')
+
+    def __str__(self):
+        return self.name
+
 class Season(models.Model):
-    number = models.PositiveIntegerField()
+    number = models.IntegerField()
     description = models.TextField()
 
     def __str__(self):
         return f"Season {self.number}"
-
-class Character(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    house = models.ForeignKey(House, related_name='characters', on_delete=models.CASCADE)
-    seasons = models.ManyToManyField(Season, related_name='characters')
-
-    def __str__(self):
-        return self.name
